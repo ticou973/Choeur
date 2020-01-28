cbd0a8ae0d3960de1debf7882095483c58a1c661
Thierry Couilleault <thierryc973@gmail.com>
2020-01-28 11:02:37 +0100

    notification pour les nouvelles chansons en ligne

---
+++ app/src/main/java/dedicace/com/ui/MyFirebaseMessagingService.java
@@ -0,0 +1,73 @@
+package dedicace.com.ui;
+
+import android.app.ActivityManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.List;
+
+import dedicace.com.R;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private static final String TAG ="coucou";
+    private static final String CANAL = "MyNotifCanal";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        String message = remoteMessage.getNotification().getBody();
+        Log.d(TAG, "MFMS onMessageReceived: "+message);
+
+        //action
+
+
+
+
+        //créer une notification
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, CANAL);
+        notificationBuilder.setContentTitle("MAJ Chansons");
+        notificationBuilder.setContentText(message);
+        notificationBuilder.setSmallIcon(R.drawable.bell);
+
+        ActivityManager activityManager = (ActivityManager) this.getSystemService(ACTIVITY_SERVICE);
+
+
+        //permet de vérifie qu'une application donné par son package est en cours
+        List<ActivityManager.RunningAppProcessInfo> procInfos = activityManager.getRunningAppProcesses();
+        for (int i = 0; i < procInfos.size(); i++) {
+            if (procInfos.get(i).processName.equals("dedicace.com")) { //ici le nom du package recherché
+                Log.d(TAG, "MFMS onMessageReceived: passage dedicace ouverte ");
+
+            }else{
+                Log.d(TAG, "MFMS onMessageReceived: passage dedicace fermé ");
+                Intent intent = new Intent(getApplicationContext(),SplashActivity.class);
+                PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,0);
+                notificationBuilder.setContentIntent(pendingIntent);
+            }
+        }
+
+        //envoyer la notification
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            String channelId = getString(R.string.notification_channel_id);
+            String channelTitle = getString(R.string.notification_channel_title);
+            String channelDescription = getString(R.string.notification_channel_description);
+
+            NotificationChannel channel = new NotificationChannel(channelId,channelTitle,NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription(channelDescription);
+            notificationManager.createNotificationChannel(channel);
+            notificationBuilder.setChannelId(channelId);
+        }
+        notificationManager.notify(1,notificationBuilder.build());
+    }
+}
+
